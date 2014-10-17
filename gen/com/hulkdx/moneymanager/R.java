151333c8a5a5fb56d4ccc474b3c147aecc0df938
hulkdx <hulktroll@gmail.com>
2014-10-17 15:50:34 +0300

    Version 1

--- gen/com/hulkdx/moneymanager/R.java
+++ gen/com/hulkdx/moneymanager/R.java
@@ -1077,10 +1077,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int category_clicked=0x7f020057;
-        public static final int category_unclicked=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
-        public static final int ico_001=0x7f02005a;
+        public static final int ic_launcher=0x7f020057;
+        public static final int ico_001=0x7f020058;
     }
     public static final class id {
         public static final int TextView01=0x7f050050;
