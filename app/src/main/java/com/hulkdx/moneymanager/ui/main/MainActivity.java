8eddc354e4097f59d2d8eac31103ab422ad6befe
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-06-13 21:21:45 +0300

    some variable names changed and old MainActivity removed.

--- app/src/main/java/com/hulkdx/moneymanager/ui/main/MainActivity.java
+++ app/src/main/java/com/hulkdx/moneymanager/ui/main/MainActivity.java
@@ -3,7 +3,11 @@ package com.hulkdx.moneymanager.ui.main;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
 import javax.inject.Inject;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import timber.log.Timber;
@@ -17,8 +21,8 @@ public class MainActivity extends BaseActivity implements MainMvpView {
             "com.hulkdx.moneymanager.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
 
     @Inject MainPresenter mMainPresenter;
-
-//    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+    @BindView(R.id.etUsername) EditText etUsername;
+    @BindView(R.id.etInitialMoney) EditText etInitialMoney;
 
     /**
      * Return an Intent to start this Activity.
@@ -49,7 +53,32 @@ public class MainActivity extends BaseActivity implements MainMvpView {
 
     @OnClick(R.id.butEnter)
     void onEnterClicked() {
-        Timber.i("yes");
+        Timber.i(etUsername.getText().toString());
+        // checking if name is empty
+//        if(nameString.isEmpty()){
+//            Toast.makeText(this, "Please provide a name", Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+//            // checking if money is empty
+//            if ( moneyString.isEmpty() || moneyString.equals("0") ) {
+//                Toast.makeText(this, "Please provide your money", Toast.LENGTH_SHORT).show();
+//            }
+//            else {
+//
+//                // Save name
+//                SharedPreferences sp = getSharedPreferences("MyDataName", Context.MODE_PRIVATE);
+//                SharedPreferences.Editor editor = sp.edit();
+//                editor.putString("name", nameString);
+//                editor.putInt("totalMoney", Integer.parseInt(moneyString));
+//                editor.commit();
+//
+//
+//                // Go to MoneyManager activity
+//                Intent i = new Intent(this, MoneyManager.class);
+//                startActivity(i);
+//                finish();
+//            }
+//        }
     }
 
     /***** MVP View methods implementation *****/
