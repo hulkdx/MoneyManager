ce590e4df074010d4802b6fc32bdfcbd18e2b637
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-07-08 14:44:39 +0300

    layout background is white Set background of the layout on odd position to white

--- app/src/main/java/com/hulkdx/moneymanager/ui/main/TransactionAdapter.java
+++ app/src/main/java/com/hulkdx/moneymanager/ui/main/TransactionAdapter.java
@@ -3,22 +3,32 @@
  */
 package com.hulkdx.moneymanager.ui.main;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 import com.hulkdx.moneymanager.R;
+import com.hulkdx.moneymanager.injection.ApplicationContext;
+
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionHolder> {
 
     private List<String> mTransactions;
+    private Context mContext;
 
     @Inject
-    public TransactionAdapter() {
+    public TransactionAdapter(@ApplicationContext Context context) {
+        mContext = context;
         mTransactions = new ArrayList<>();
         mTransactions.add("saba");
         mTransactions.add("saba");
@@ -37,7 +47,10 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
 
     @Override
     public void onBindViewHolder(TransactionHolder holder, int position) {
-
+        // Set background of the layout on odd position to white
+        if (position % 2 == 1) {
+            holder.rootLayout.setBackgroundColor(ContextCompat.getColor(mContext, R.color.white));
+        }
     }
 
     @Override
@@ -46,7 +59,7 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     }
 
     class TransactionHolder extends RecyclerView.ViewHolder {
-//        @BindView(R.id.text_name) public TextView name;
+        @BindView(R.id.root_layout) public LinearLayout rootLayout;
 
         public TransactionHolder(View itemView) {
             super(itemView);
