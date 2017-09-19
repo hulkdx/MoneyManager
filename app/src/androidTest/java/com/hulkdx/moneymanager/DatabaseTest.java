0f1fcad159c2bf54ccab38aba7e88a1a7de8ab1e
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-09-19 19:38:52 +0300

    test get transactions.

--- app/src/androidTest/java/com/hulkdx/moneymanager/DatabaseTest.java
+++ app/src/androidTest/java/com/hulkdx/moneymanager/DatabaseTest.java
@@ -12,9 +12,19 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
+
+import static com.hulkdx.moneymanager.common.TestDataFactory.makeListTransactions;
 
 /**
  * Created by Mohammad Jafarzadeh Rezvan on 19/09/2017.
@@ -63,6 +73,39 @@ public class DatabaseTest extends TestCase {
                     assertEquals(result.getAmount(), 0f);
                     assertEquals(result.getDate(), "1999-9-9");
                     assertEquals(result.getAttachment(), "test");
+                });
+    }
+
+    @Test
+    public void testGetTransactions(){
+        mRealm.executeTransactionAsync(
+                bgRealm -> {
+                    List<Transaction> transactions = makeListTransactions(10);
+                    // 1. Add a list of transactions
+                    for (Transaction newTransaction: transactions) {
+                        // Auto Incremental Id
+                        Number currentIdNum = bgRealm.where(Transaction.class).max("id");
+                        int nextId = currentIdNum == null ? 1 : currentIdNum.intValue() + 1;
+                        newTransaction.setId(nextId);
+
+                        bgRealm.copyToRealm(newTransaction);
+                    }
+
+
+                    // Test the result.
+                    RealmResults<Transaction> result = mRealm.where(Transaction.class).findAll();
+                    int pos = 0;
+                    for (Transaction resultTransaction: result) {
+                        assertEquals(resultTransaction.getAmount(), pos);
+
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                        Date todayDate = new Date();
+                        Date plusDate = new Date(todayDate.getTime() + TimeUnit.DAYS.toMillis( pos ));
+
+                        assertEquals(resultTransaction.getDate(), dateFormat.format(plusDate));
+                        pos++;
+                    }
+
                 });
     }
 }
