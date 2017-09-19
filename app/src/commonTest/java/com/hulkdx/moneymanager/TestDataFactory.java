f85b5b92d5bec74cfd46403e11ed39cba85bc842
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-09-19 14:08:15 +0300

    removed AUTOVALUE AND GLIDE and added Espresso tests to the project.

---
+++ app/src/commonTest/java/com/hulkdx/moneymanager/TestDataFactory.java
@@ -0,0 +1,33 @@
+package com.hulkdx.moneymanager;
+
+import com.hulkdx.moneymanager.data.model.Transaction;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Mohammad Jafarzadeh Rezvan on 19/09/2017.
+ */
+
+public class TestDataFactory {
+
+    public static List<Transaction> makeListTransactions(int number) {
+        List<Transaction> transactions = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            transactions.add(makeTransactionWithoutCategory(i));
+        }
+        return transactions;
+    }
+
+    private static Transaction makeTransactionWithoutCategory(int i) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        Date todayDate = new Date();
+        Date plusDate = new Date(todayDate.getTime() + TimeUnit.DAYS.toMillis( i ));
+        return new Transaction(dateFormat.format(plusDate), i+1, "");
+    }
+}
