f7d3c9b86dd8b91902d88ded917b4818d55eee5e
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2019-02-23 20:09:22 +0200

    From the begining.

--- app/src/main/java/com/hulkdx/moneymanagerv2/data/SyncServiceListener.java
+++
@@ -1,9 +0,0 @@
-package com.hulkdx.moneymanagerv2.data;
-
-/**
- * Created by Mohammad Jafarzadeh Rezvan on 26/12/2017.
- */
-
-public interface SyncServiceListener {
-    void finishAndStartChooser();
-}
--- app/src/main/java/com/hulkdx/moneymanagerv2/ui/login/LoginMvpView.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Mohammad Jafarzadeh Rezvan on 6/13/2017.
- */
-package com.hulkdx.moneymanagerv2.ui.login;
-
-import com.hulkdx.moneymanagerv2.ui.base.MvpView;
-
-public interface LoginMvpView extends MvpView {
-}
